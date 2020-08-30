834ae22f19402ec2382562602e68acf9c3adcc09
gloriakim-cs-projects <gloriakim.cs.projects@gmail.com>
2020-08-30 05:15:23 -0500

    Completed the step 3's details more &the step 4

--- summer_escape/lib/screens/step_four_details.dart
+++ summer_escape/lib/screens/step_four_details.dart
@@ -1,8 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:summer_escape/screens/step.dart';
 
 class StepFourDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green,);
+    return new Scaffold(
+      //show background
+      extendBodyBehindAppBar: true,
+      body: Container(
+        //show full-size background
+        decoration: BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage('images/landing_image.JPG'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: MaterialButton(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0)),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => StepPage(
+                                58688,
+                                'STEP 4.',
+                                'ENJOY\nYOUR\nVACATION.',
+                                StepFourDetailsPage(),
+                              ),
+                            ),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: const Text(
+                            'Landing...',
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.blue,
+                              fontFamily: 'Shrikhand',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- summer_escape/lib/screens/step_three_details_more.dart
+++ summer_escape/lib/screens/step_three_details_more.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:summer_escape/screens/step.dart';
+import 'package:summer_escape/screens/step_four_details.dart';
 
 class StepThreeDetailsMore extends StatefulWidget {
   @override
@@ -8,6 +10,65 @@ class StepThreeDetailsMore extends StatefulWidget {
 class _StepThreeDetailsMoreState extends State<StepThreeDetailsMore> {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green);
+    return new Scaffold(
+      //show background
+      extendBodyBehindAppBar: true,
+      body: Container(
+        //show full-size background
+        decoration: BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage('images/sky_image.JPG'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: MaterialButton(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0)),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => StepPage(
+                                58688,
+                                'STEP 4.',
+                                'ENJOY\nYOUR\nVACATION.',
+                                StepFourDetailsPage(),
+                              ),
+                            ),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: const Text(
+                            'Arriving in 3 Seconds...',
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.blue,
+                              fontFamily: 'Shrikhand',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
