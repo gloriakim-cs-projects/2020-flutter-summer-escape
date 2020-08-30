f1c3fcbbc18005927a1c0d7be7a9262bd1d3975c
gloriakim-cs-projects <gloriakim.cs.projects@gmail.com>
2020-08-29 22:29:46 -0500

    Completed the design of setting

--- summer_escape/lib/screens/credits.dart
+++ summer_escape/lib/screens/credits.dart
@@ -7,6 +7,20 @@ class CreditsPage extends StatelessWidget {
     return new Scaffold(
       //show background
       extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: const Text(
+          'CREDITS',
+          style: TextStyle(
+            decoration: TextDecoration.underline,
+            fontSize: 50.0,
+            color: Colors.white,
+            fontFamily: 'Shrikhand',
+          ),
+        ),
+      ),
       body: Container(
         //show full-size background
         decoration: BoxDecoration(
@@ -20,26 +34,16 @@ class CreditsPage extends StatelessWidget {
         ),
         //show icons
         child: SafeArea(
-          child: SingleChildScrollView(
-            child: Stack(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+            child: SingleChildScrollView(
+              child: Stack(
+                children: <Widget>[
+                  Container(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        const Text(
-                          'CREDITS',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            fontSize: 50.0,
-                            color: Colors.white,
-                            fontFamily: 'Shrikhand',
-                          ),
-                        ),
                         const Text('PHOTOS',
                             style: TextStyle(
                               fontSize: 30,
@@ -48,20 +52,21 @@ class CreditsPage extends StatelessWidget {
                             )),
                         MyButton.getWebpage(
                             context,
-                            'Safari Photo by AJ Robbie on Unsplash',
+                            '- Safari Photo by AJ Robbie on Unsplash',
                             'https://unsplash.com/photos/BuQ1RZckYW4'),
                         MyButton.getWebpage(
                             context,
-                            'Ocean Photo by Sean O. on Unsplash',
+                            '- Ocean Photo by Sean O. on Unsplash',
                             'https://unsplash.com/photos/KMn4VEeEPR8'),
                         MyButton.getWebpage(
                             context,
-                            'Mountain Photo by Kalen Emsley on Unsplash',
+                            '- Mountain Photo by Kalen Emsley on Unsplash',
                             'https://unsplash.com/photos/Bkci_8qcdvQ'),
                         MyButton.getWebpage(
                             context,
-                            'Forest Photo by Imat Bagja Gumilar on Unsplash',
+                            '- Forest Photo by Imat Bagja Gumilar on Unsplash',
                             'https://unsplash.com/photos/jFCViYFYcus'),
+                        SizedBox(height: 10),
                         const Text('Illustrations',
                             style: TextStyle(
                               fontSize: 30,
@@ -70,22 +75,25 @@ class CreditsPage extends StatelessWidget {
                             )),
                         MyButton.getWebpage(
                             context,
-                            'Escalator Vectors by Vecteezy',
+                            '- Escalator Vectors by Vecteezy',
                             'https://www.vecteezy.com/vector-art/144491-escalator-airport-free-vector'),
                         MyButton.getWebpage(
                             context,
-                            'Open Suitcase Vectors by Vecteezy',
+                            '- Open Suitcase Vectors by Vecteezy',
                             'https://www.vecteezy.com/vector-art/94571-free-open-suitcase-vector'),
                         MyButton.getWebpage(
                             context,
-                            'Pilot Vectors by Vecteezy',
+                            '- Pilot Vectors by Vecteezy',
                             'https://www.vecteezy.com/vector-art/106204-vector-plane-window'),
-                        MyButton.getWebpage(context, 'Pool Vectors by Vecteezy',
+                        MyButton.getWebpage(
+                            context,
+                            '- Pool Vectors by Vecteezy',
                             'https://www.vecteezy.com/vector-art/210841-vintage-swimming-pool-vector'),
                         MyButton.getWebpage(
                             context,
-                            'Suitcase Vectors by Vecteezy',
+                            '- Suitcase Vectors by Vecteezy',
                             'https://www.vecteezy.com/vector-art/215850-luggage-vector'),
+                        SizedBox(height: 10),
                         const Text('Icons',
                             style: TextStyle(
                               fontSize: 30,
@@ -94,17 +102,17 @@ class CreditsPage extends StatelessWidget {
                             )),
                         MyButton.getWebpage(
                             context,
-                            'Earth, Suitcase, Airplane, Vacation Icons\ by Freepik from www.flaticon.com',
+                            '- Earth, Suitcase, Airplane, Vacation Icons\ by Freepik from flaticon.com',
                             'https://www.flaticon.com/authors/freepik'),
                         MyButton.getWebpage(
                             context,
-                            'Speaker Icon by Pixel perfect from www.flaticon.com',
+                            '- Speaker Icon by Pixel perfect from flaticon.com',
                             'https://www.flaticon.com/authors/pixel-perfect'),
                       ],
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
@@ -116,14 +124,14 @@ class CreditsPage extends StatelessWidget {
 class MyButton {
   static getWebpage(BuildContext context, String string, String url) {
     return FlatButton(
-      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+//      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
       onPressed: () => _launchURL(url),
       child: Align(
         alignment: Alignment.centerLeft,
         child: Text(
           string,
           style: TextStyle(
-            fontSize: 15,
+            fontSize: 20,
             color: Colors.white,
             fontFamily: 'CarterOne',
           ),
--- summer_escape/lib/screens/settings.dart
+++ summer_escape/lib/screens/settings.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  //variable for checkbox
+  Map<String, bool> values = {
+    'Light Mode': false,
+    'Dark Mode': false,
+  };
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return new Scaffold(
+      //show background
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: const Text(
+          'SETTINGS',
+          style: TextStyle(
+            decoration: TextDecoration.underline,
+            fontSize: 50.0,
+            color: Colors.white,
+            fontFamily: 'Shrikhand',
+          ),
+        ),
+      ),
+      body: Container(
+        //show full-size background
+        decoration: BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage('images/background_image.JPG'),
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.6), BlendMode.dstATop),
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+          child: ListView(
+            children: values.keys.map((String key) {
+              return new CheckboxListTile(
+                title: Text(
+                  key,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontFamily: 'CarterOne',
+                  ),
+                ),
+                value: values[key],
+                onChanged: (bool value) {
+                  setState(() {
+                    values[key] = value;
+                  });
+                },
+                //leading checkbox
+                controlAffinity: ListTileControlAffinity.leading,
+                checkColor: Colors.blue,
+                activeColor: Colors.white,
+              );
+            }).toList(),
+          ),
+        ),
+      ),
     );
   }
 }
