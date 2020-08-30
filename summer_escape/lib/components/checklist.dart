37f25998510ab79ec584be15a770dab5482e02ea
gloriakim-cs-projects <gloriakim.cs.projects@gmail.com>
2020-08-30 02:55:32 -0500

    Completed the design of checklist

--- summer_escape/lib/components/checklist.dart
+++ summer_escape/lib/components/checklist.dart
@@ -6,10 +6,67 @@ class CheckListPage extends StatefulWidget {
 }
 
 class _CheckListPageState extends State<CheckListPage> {
+  //variable for checkbox
+  Map<String, bool> values = {
+    'Towel': false,
+    'Clothes': false,
+    'Hats': false,
+    'Shoes': false,
+    'Toothbrush': false,
+    'Cell Phone': false,
+    'Pillow': false,
+    'Cash': false,
+    'Passport': false,
+    'Brush': false,
+  };
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.purple,
+    return new Scaffold(
+      //show background
+      backgroundColor: Colors.blue,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: const Text(
+          'CHECKLIST',
+          style: TextStyle(
+            decoration: TextDecoration.underline,
+            fontSize: 50.0,
+            color: Colors.white,
+            fontFamily: 'Shrikhand',
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+        child: ListView(
+          children: values.keys.map((String key) {
+            return new CheckboxListTile(
+              //TODO (Optional): Build the actual mode changes
+              title: Text(
+                key,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontFamily: 'CarterOne',
+                ),
+              ),
+              value: values[key],
+              onChanged: (bool value) {
+                setState(() {
+                  values[key] = value;
+                });
+              },
+              //leading checkbox
+              controlAffinity: ListTileControlAffinity.leading,
+              checkColor: Colors.blue,
+              activeColor: Colors.white,
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
