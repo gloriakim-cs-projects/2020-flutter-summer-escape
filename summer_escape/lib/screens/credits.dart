9bdefa758e6cc30ab7d86d5cabb55161890e0348
gloriakim-cs-projects <gloriakim.cs.projects@gmail.com>
2020-08-29 21:50:37 -0500

    Completed the credit page

--- summer_escape/lib/screens/credits.dart
+++ summer_escape/lib/screens/credits.dart
@@ -5,7 +5,7 @@ class CreditsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      //show background behind the app bar
+      //show background
       extendBodyBehindAppBar: true,
       body: Container(
         //show full-size background
@@ -13,52 +13,94 @@ class CreditsPage extends StatelessWidget {
           color: Colors.black,
           image: DecorationImage(
             image: AssetImage('images/background_image.JPG'),
-            //TODO: opacity!
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.6), BlendMode.dstATop),
             fit: BoxFit.cover,
           ),
         ),
         //show icons
-        child: Expanded(
-          child: SafeArea(
-            child: Column(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Stack(
               children: <Widget>[
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(0,20,0,0),
-                  child: Text(
-                    'CREDITS',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 50.0,
-                      color: Colors.white,
-                      fontFamily: 'Shrikhand',
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      color: Colors.red,
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          const Text('PHOTOS',
-                              style: TextStyle(
-                                fontSize: 30,
-                                color: Colors.white,
-                                fontFamily: 'CarterOne',
-                              )),
-                          RaisedButton(
-                            onPressed: _launchURL,
-                            child: Text('Photo by AJ Robbie on Unsplash'),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const Text(
+                          'CREDITS',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 50.0,
+                            color: Colors.white,
+                            fontFamily: 'Shrikhand',
                           ),
-
-                        ],
-                      ),
+                        ),
+                        const Text('PHOTOS',
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontFamily: 'CarterOne',
+                            )),
+                        MyButton.getWebpage(
+                            context,
+                            'Safari Photo by AJ Robbie on Unsplash',
+                            'https://unsplash.com/photos/BuQ1RZckYW4'),
+                        MyButton.getWebpage(
+                            context,
+                            'Ocean Photo by Sean O. on Unsplash',
+                            'https://unsplash.com/photos/KMn4VEeEPR8'),
+                        MyButton.getWebpage(
+                            context,
+                            'Mountain Photo by Kalen Emsley on Unsplash',
+                            'https://unsplash.com/photos/Bkci_8qcdvQ'),
+                        MyButton.getWebpage(
+                            context,
+                            'Forest Photo by Imat Bagja Gumilar on Unsplash',
+                            'https://unsplash.com/photos/jFCViYFYcus'),
+                        const Text('Illustrations',
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontFamily: 'CarterOne',
+                            )),
+                        MyButton.getWebpage(
+                            context,
+                            'Escalator Vectors by Vecteezy',
+                            'https://www.vecteezy.com/vector-art/144491-escalator-airport-free-vector'),
+                        MyButton.getWebpage(
+                            context,
+                            'Open Suitcase Vectors by Vecteezy',
+                            'https://www.vecteezy.com/vector-art/94571-free-open-suitcase-vector'),
+                        MyButton.getWebpage(
+                            context,
+                            'Pilot Vectors by Vecteezy',
+                            'https://www.vecteezy.com/vector-art/106204-vector-plane-window'),
+                        MyButton.getWebpage(context, 'Pool Vectors by Vecteezy',
+                            'https://www.vecteezy.com/vector-art/210841-vintage-swimming-pool-vector'),
+                        MyButton.getWebpage(
+                            context,
+                            'Suitcase Vectors by Vecteezy',
+                            'https://www.vecteezy.com/vector-art/215850-luggage-vector'),
+                        const Text('Icons',
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontFamily: 'CarterOne',
+                            )),
+                        MyButton.getWebpage(
+                            context,
+                            'Earth, Suitcase, Airplane, Vacation Icons\ by Freepik from www.flaticon.com',
+                            'https://www.flaticon.com/authors/freepik'),
+                        MyButton.getWebpage(
+                            context,
+                            'Speaker Icon by Pixel perfect from www.flaticon.com',
+                            'https://www.flaticon.com/authors/pixel-perfect'),
+                      ],
                     ),
                   ),
                 ),
@@ -71,8 +113,27 @@ class CreditsPage extends StatelessWidget {
   }
 }
 
-_launchURL() async {
-  const url = 'https://unsplash.com/photos/BuQ1RZckYW4';
+class MyButton {
+  static getWebpage(BuildContext context, String string, String url) {
+    return FlatButton(
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      onPressed: () => _launchURL(url),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          string,
+          style: TextStyle(
+            fontSize: 15,
+            color: Colors.white,
+            fontFamily: 'CarterOne',
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+_launchURL(String url) async {
   if (await canLaunch(url)) {
     await launch(url);
   } else {
--- summer_escape/lib/screens/home.dart
+++ summer_escape/lib/screens/home.dart
@@ -14,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      //show background behind the app bar
+      //show background
       extendBodyBehindAppBar: true,
       body: Container(
         //show full-size background
@@ -26,129 +26,128 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
         //show icons
-        child: Expanded(
-          child: SafeArea(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0,40,0,0),
-                  child: Text(
-                    'SUMMER ESCAPE',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 70.0,
-                      color: Color(0xff6BBCED),
-                      fontFamily: 'Shrikhand',
-                      shadows: <Shadow>[
-                        //actual shadow
-                        Shadow(
-                          offset: Offset(5.0, 5.0),
-                          color: Color(0xFF01579B),
-                        ),
-                        //outline
-            Shadow( // bottomLeft
-                offset: Offset(-1.5, -1.5),
-                color: Colors.white
-            ),
-            Shadow( // bottomRight
-                offset: Offset(1.5, -1.5),
-                color: Colors.white
-            ),
-            Shadow( // topRight
-                offset: Offset(1.5, 1.5),
-                color: Colors.white
-            ),
-            Shadow( // topLeft
-              offset: Offset(-1.5, 1.5),
-              color: Colors.white),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        child: MaterialButton(
-                          color: Colors.white,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(100.0)),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => CreditsPage()),
-                            );
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: const Text('CREDITS',
-                                style: TextStyle(
-                                  fontSize: 30,
-
-                                  color: Color(0xff6BBCED),
-                                  fontFamily: 'CarterOne',
-                                )),
-                          ),
-                        ),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                child: Text(
+                  'SUMMER ESCAPE',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 70.0,
+                    color: Color(0xff6BBCED),
+                    fontFamily: 'Shrikhand',
+                    shadows: <Shadow>[
+                      //actual shadow
+                      Shadow(
+                        offset: Offset(5.0, 5.0),
+                        color: Color(0xFF01579B),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 50),
-                        child: MaterialButton(
-                          color: Colors.white,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(100.0)),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SettingsPage()),
-                            );
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: const Text('SETTINGS',
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  color: Color(0xff6BBCED),
-                                  fontFamily: 'CarterOne',
-                                )),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 50),
-                        child: MaterialButton(
-                          color: Color(0xff6BBCED),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(100.0)),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => StepOnePage()),
-                            );
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: const Text('START',
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  color: Colors.white,
-                                  fontFamily: 'CarterOne',
-                                )),
-                          ),
-                        ),
-                      ),
+                      //outline
+                      Shadow(
+                          // bottomLeft
+                          offset: Offset(-1.5, -1.5),
+                          color: Colors.white),
+                      Shadow(
+                          // bottomRight
+                          offset: Offset(1.5, -1.5),
+                          color: Colors.white),
+                      Shadow(
+                          // topRight
+                          offset: Offset(1.5, 1.5),
+                          color: Colors.white),
+                      Shadow(
+                          // topLeft
+                          offset: Offset(-1.5, 1.5),
+                          color: Colors.white),
                     ],
                   ),
                 ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: MaterialButton(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100.0)),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CreditsPage()),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: const Text('CREDITS',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Color(0xff6BBCED),
+                                fontFamily: 'CarterOne',
+                              )),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 50),
+                      child: MaterialButton(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100.0)),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SettingsPage()),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: const Text('SETTINGS',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Color(0xff6BBCED),
+                                fontFamily: 'CarterOne',
+                              )),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 50),
+                      child: MaterialButton(
+                        color: Color(0xff6BBCED),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100.0)),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => StepOnePage()),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: const Text('START',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                                fontFamily: 'CarterOne',
+                              )),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
 //              Padding(
 //                padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
 //                child: MaterialButton(
@@ -195,8 +194,7 @@ class _HomePageState extends State<HomePage> {
 //                  ),
 //                ),
 //              ),
-              ],
-            ),
+            ],
           ),
         ),
       ),
