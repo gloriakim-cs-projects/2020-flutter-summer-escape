3b95c1b8a281852ff0daf126616e46ac2d1510a8
gloriakim-cs-projects <gloriakim.cs.projects@gmail.com>
2020-08-30 01:22:25 -0500

    Removed the search bar

--- summer_escape/lib/screens/ExamplePage.dart
+++ summer_escape/lib/screens/ExamplePage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class StepFourDetailsPage extends StatelessWidget {
+class ExamplePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
--- summer_escape/lib/screens/step_one_details.dart
+++ summer_escape/lib/screens/step_one_details.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:summer_escape/screens/ExamplePage.dart.dart';
+import 'package:summer_escape/screens/ExamplePage.dart';
 
 class Destination {
   final String title;
@@ -27,97 +27,64 @@ class FirstPageDetails extends StatefulWidget {
 }
 
 class _FirstPageDetailsState extends State<FirstPageDetails> {
-  //for search bar's text edit
-  TextEditingController _textController = TextEditingController();
-
-  onItemChanged(String value) {
-    setState(() {
-      dest = dest
-          .where((string) =>
-              string.title.toLowerCase().contains(value.toLowerCase()) ||
-              string.description.toLowerCase().contains(value.toLowerCase()))
-          .toList();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Theme(
-          data: Theme.of(context).copyWith(accentColor: Colors.transparent),
-          child: TextField(
-            style: TextStyle(color: Colors.black),
-            controller: _textController,
-            decoration: InputDecoration(
-              prefixIcon: new Icon(Icons.search, color: Colors.black),
-              hintText: 'Search Here',
-              hintStyle: TextStyle(fontSize: 20.0, color: Colors.black),
-            ),
-            onChanged: onItemChanged,
-          ),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        automaticallyImplyLeading: false,
-      ),
+      backgroundColor: Colors.white,
       //show background behind the app bar
       extendBodyBehindAppBar: true,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-        child: SafeArea(
-          child: Expanded(
-            child: ListView.builder(
-              itemCount: dest.length,
-              itemBuilder: (context, index) {
-                String imagename = dest[index].title;
-                return Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  child: Container(
-                    height: 120,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      image: DecorationImage(
-                        image: AssetImage('images/$imagename.jpg'),
-                        colorFilter: ColorFilter.mode(
-                            Colors.black.withOpacity(0.6), BlendMode.dstATop),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: Center(
-                      child: ListTile(
-                        title: Text(
-                          dest[index].title,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'Shrikhand',
-                            fontSize: 40,
-                          ),
-                        ),
-                        subtitle: Text(
-                          dest[index].description,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'Shrikhand',
-                            fontSize: 20,
-                          ),
-                        ),
-                        trailing: Icon(Icons.keyboard_arrow_right,
-                            size: 60.0, color: Colors.white),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ExamplePage(),
-                            ),
-                          );
-                        },
-                      ),
+      body: SafeArea(
+        child: Expanded(
+          child: ListView.builder(
+            itemCount: dest.length,
+            itemBuilder: (context, index) {
+              String imagename = dest[index].title;
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                child: Container(
+                  height: 120,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    image: DecorationImage(
+                      image: AssetImage('images/$imagename.jpg'),
+                      colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.6), BlendMode.dstATop),
+                      fit: BoxFit.cover,
                     ),
                   ),
-                );
-              },
-            ),
+                  child: Center(
+                    child: ListTile(
+                      title: Text(
+                        dest[index].title,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Shrikhand',
+                          fontSize: 40,
+                        ),
+                      ),
+                      subtitle: Text(
+                        dest[index].description,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Shrikhand',
+                          fontSize: 20,
+                        ),
+                      ),
+                      trailing: Icon(Icons.keyboard_arrow_right,
+                          size: 60.0, color: Colors.white),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ExamplePage(),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              );
+            },
           ),
         ),
       ),
